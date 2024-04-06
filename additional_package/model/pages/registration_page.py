c5451aa5b91eadf8afd026f23d4ee7a6dea843c4
neverov.iv@gmail.com <neverov.iv@gmail.com>
2024-04-06 16:06:40 +0300

    Comment for branch mid-level-step-objects

---
+++ additional_package/model/pages/registration_page.py
@@ -0,0 +1,69 @@
+from selene import have, command
+from selene.support.shared import browser
+from additional_package import resource
+
+class RegistrationPage:
+
+    def open(self):
+        browser.open('/automation-practice-form')
+
+    def fill_first_name(self, value):
+        browser.element('#firstName').type(value)
+
+    def fill_lastname(self, value):
+        browser.element('#lastName').type(value)
+
+    def fill_email(self, value):
+        browser.element('#userEmail').type(value)
+
+    def fill_gender(self):
+        browser.element('[for="gender-radio-1"]').click()
+
+    def fill_number(self, value):
+        browser.element('#userNumber').type(value)
+
+    def fill_birthdate(self, year=9, month=8, date=8):
+        if date <= 9:
+            date = '00' + str(date)
+        else:
+            date = '0' + str(date)
+        browser.element('#dateOfBirthInput').click()
+        browser.element('.react-datepicker__year-select').click()
+        browser.element(f'[value="{year}"]').click()
+        browser.element('.react-datepicker__month-select').click()
+        browser.element(f'[value="{month}"]').click()
+        browser.element(f'[class="react-datepicker__day react-datepicker__day--{date}"]').click()
+
+    def fill_subject(self, value):
+        browser.element('#subjectsInput').type(value).press_enter()
+        browser.element('#subjectsInput').perform(command.js.scroll_into_view)
+
+    def choose_hobbies_checbox(self, value=1):
+        browser.element(f'[for="hobbies-checkbox-{value}"]').click()
+
+    def upload_picture(self, name):
+        browser.element('#uploadPicture').set_value(resource.path(name))
+
+    def fill_current_address(self, value):
+        browser.element('#currentAddress').type(value)
+
+    def choose_state(self, value='NCR'):
+        browser.element('#react-select-3-input').type(value).press_enter()
+
+    def fill_city(self, value='Delhi'):
+        browser.element('#react-select-4-input').type(value).press_enter()
+
+    def submit_registration(self):
+        browser.element('#submit').click()
+
+    def assert_registered_info(self, name, email, sex, number, birthday, subject, hobby, file_name, address, state_and_city):
+        browser.element('[class="modal-content"]').should(have.text(name))
+        browser.element('[class="modal-content"]').should(have.text(email))
+        browser.element('[class="modal-content"]').should(have.text(sex))
+        browser.element('[class="modal-content"]').should(have.text(number))
+        browser.element('[class="modal-content"]').should(have.text(birthday))
+        browser.element('[class="modal-content"]').should(have.text(subject))
+        browser.element('[class="modal-content"]').should(have.text(hobby))
+        browser.element('[class="modal-content"]').should(have.text(file_name))
+        browser.element('[class="modal-content"]').should(have.text(address))
+        browser.element('[class="modal-content"]').should(have.text(state_and_city))
--- tests/test_form.py
+++ tests/test_form.py
@@ -1,37 +1,34 @@
 from selene import browser, have, command
-
+from additional_package.model.pages.registration_page import RegistrationPage
 from additional_package import resource
 
 
 def test_fill_form(browser_personal_settings):
-    browser.open('/automation-practice-form')
-    browser.element('#firstName').type('Ivan')
-    browser.element('#lastName').type('Neverov')
-    browser.element('#userEmail').type('neverovsky@mail.ru')
-    browser.element('[for="gender-radio-1"]').click()
-    browser.element('#userNumber').type('8927654341')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__year-select').click()
-    browser.element('[value="1999"]').click()
-    browser.element('.react-datepicker__month-select').click()
-    browser.element('[value="8"]').click()
-    browser.element('[class="react-datepicker__day react-datepicker__day--008"]').click()
-    browser.element('#subjectsInput').type('Physics').press_enter()
-    browser.element('#subjectsInput').perform(command.js.scroll_into_view)
-    browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('#uploadPicture').set_value(resource.path('1.jpg'))
-    browser.element('#currentAddress').type('Saint-Petersburg')
-    browser.element('#react-select-3-input').type('NCR').press_enter()
-    browser.element('#react-select-4-input').type('Delhi').press_enter()
-    browser.element('#submit').click()
+    registration_page = RegistrationPage()
+    registration_page.open()
+    registration_page.fill_first_name('Ivan')
+    registration_page.fill_lastname('Neverov')
+    registration_page.fill_email('neverovsky@mail.ru')
+    registration_page.fill_gender()
+    registration_page.fill_number('8927654341')
+    registration_page.fill_birthdate(1999, 8, 8)
+    registration_page.fill_subject('Physics')
+    registration_page.choose_hobbies_checbox(1)
+    registration_page.upload_picture('1.jpg')
+    registration_page.fill_current_address('Saint-Petersburg')
+    registration_page.choose_state('NCR')
+    registration_page.fill_city('Delhi')
+    registration_page.submit_registration()
 
-    browser.element('[class="modal-content"]').should(have.text('Ivan Neverov'))
-    browser.element('[class="modal-content"]').should(have.text('neverovsky@mail.ru'))
-    browser.element('[class="modal-content"]').should(have.text('Male'))
-    browser.element('[class="modal-content"]').should(have.text('8927654341'))
-    browser.element('[class="modal-content"]').should(have.text('08 September,1999'))
-    browser.element('[class="modal-content"]').should(have.text('Physics'))
-    browser.element('[class="modal-content"]').should(have.text('Sports'))
-    browser.element('[class="modal-content"]').should(have.text('1.jpg'))
-    browser.element('[class="modal-content"]').should(have.text('Saint-Petersburg'))
-    browser.element('[class="modal-content"]').should(have.text('NCR Delhi'))
+    registration_page.assert_registered_info(
+        'Ivan Neverov',
+        'neverovsky@mail.ru',
+        'Male',
+        '8927654341',
+        '08 September,1999',
+        'Physics',
+        'Sports',
+        '1.jpg',
+        'Saint-Petersburg',
+        'NCR Delhi'
+    )
